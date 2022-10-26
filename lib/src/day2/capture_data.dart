ef26b4f3b9a866c8e7189514602398069410d57d
DavidG2000 <you@example.com>
2022-10-26 17:21:42 -0400

    validando y capturando data

--- lib/src/day2/capture_data.dart
+++ lib/src/day2/capture_data.dart
@@ -41,45 +41,11 @@ class _CaptureDataState extends State<CaptureData> {
                 "Captura de datos usando TextFields",
                 style: Theme.of(context).textTheme.headline6,
               ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextField(
-                  controller: _nameController,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Nombre',
-                  ),
-                ),
+              _TextFields(
+                nameController: _nameController,
+                lastNameController: _lastNameController,
+                ageController: _ageController,
               ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextField(
-                  controller: _nameController,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Apellido',
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextField(
-                  controller: _nameController,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Edad',
-                  ),
-                ),
-              ),
-              ElevatedButton(
-                  style: ButtonStyle(
-                    backgroundColor:
-                        MaterialStateProperty.all(const Color(0xFF6750A4)),
-                    minimumSize: MaterialStateProperty.all(const Size(200, 50)),
-                  ),
-                  onPressed: () {},
-                  child: const Text('Enviar',
-                      style: TextStyle(color: Colors.white))),
               Padding(
                 padding: const EdgeInsets.only(top: 8.0),
                 child: Text("Captura de datos usando Form",
@@ -95,6 +61,91 @@ class _CaptureDataState extends State<CaptureData> {
         ),
       ),
     );
+  }
+}
+
+class _TextFields extends StatefulWidget {
+  const _TextFields(
+      {required this.nameController,
+      required this.lastNameController,
+      required this.ageController});
+  final TextEditingController nameController;
+  final TextEditingController lastNameController;
+  final TextEditingController ageController;
+
+  @override
+  State<_TextFields> createState() => _TextFieldsState();
+}
+
+class _TextFieldsState extends State<_TextFields> {
+  bool _nameIsInvalid = false;
+
+  bool _lasNameIsInvalid = false;
+
+  bool _ageIsInvalid = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextField(
+            controller: widget.nameController,
+            decoration: InputDecoration(
+              errorText: _nameIsInvalid ? "Nombre inválido" : null,
+              border: const OutlineInputBorder(),
+              labelText: 'Nombre',
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextField(
+            controller: widget.lastNameController,
+            decoration: InputDecoration(
+              errorText: _lasNameIsInvalid ? 'Apellido inválido' : null,
+              border: const OutlineInputBorder(),
+              labelText: 'Apellido',
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextField(
+            controller: widget.ageController,
+            decoration: InputDecoration(
+              errorText: _ageIsInvalid ? "Edad inválida" : null,
+              border: const OutlineInputBorder(),
+              labelText: 'Edad',
+            ),
+          ),
+        ),
+        ElevatedButton(
+            style: ButtonStyle(
+              backgroundColor:
+                  MaterialStateProperty.all(const Color(0xFF6750A4)),
+              minimumSize: MaterialStateProperty.all(const Size(200, 50)),
+            ),
+            onPressed: () => _validate(),
+            child: const Text('Enviar', style: TextStyle(color: Colors.white))),
+      ],
+    );
+  }
+
+  void _validate() {
+    setState(() {
+      _nameIsInvalid = widget.nameController.text.isEmpty;
+      _lasNameIsInvalid = widget.lastNameController.text.isEmpty;
+      _ageIsInvalid = widget.ageController.text.isEmpty;
+    });
+    // show a snackbar with the data
+    if (!_nameIsInvalid && !_lasNameIsInvalid && !_ageIsInvalid) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text(
+            "Nombre: ${widget.nameController.text} ${widget.lastNameController.text} Edad: ${widget.ageController.text}"),
+      ));
+    }
   }
 }
 
